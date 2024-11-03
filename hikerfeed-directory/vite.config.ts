c92041ac14cd5af439ceb99984507753b19cc039
Kyle Huber <kyhuber@gmail.com>
2024-11-02 21:01:11 -0700

    Update npm

--- hikerfeed-directory/vite.config.ts
+++ hikerfeed-directory/vite.config.ts
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vite.dev/config/
 export default defineConfig({
-    base: '/hikerfeed-directory/', // Should match your repository name
+    base: '/hikerfeed-team/', // Should match your repository name
     plugins: [vue()]
 })
 
