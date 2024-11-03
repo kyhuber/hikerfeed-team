3a4a0ac684166915751213a79917a01e0dd56a28
Kyle Huber <kyhuber@gmail.com>
2024-11-03 01:40:03 -0700

    new route

--- vite.config.ts
+++ vite.config.ts
@@ -12,12 +12,11 @@ export default defineConfig({
   },
   build: {
     outDir: 'dist',
-    assetsDir: 'assets',
     rollupOptions: {
       output: {
-        entryFileNames: `assets/[name].[hash].js`,
-        chunkFileNames: `assets/[name].[hash].js`,
-        assetFileNames: `assets/[name].[hash].[ext]`
+        entryFileNames: 'assets/index.js',
+        chunkFileNames: 'assets/[name].[hash].js',
+        assetFileNames: 'assets/[name].[ext]'
       }
     }
   }
