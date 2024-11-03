02a90acf2e4a7bda592327219459fbc66c536f86
Kyle Huber <kyhuber@gmail.com>
2024-11-03 01:02:33 -0700

    fex deploy issue

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import vue from '@vitejs/plugin-vue'
 import path from 'path'
 
 export default defineConfig({
-  base: '/hikerfeed_team/',  // Update this to match your repository name exactly
+  base: '/hikerfeed-team/',
   plugins: [vue()],
   resolve: {
     alias: {
